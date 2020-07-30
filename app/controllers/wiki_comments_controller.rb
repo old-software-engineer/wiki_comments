52d94a5efa818f58e5a7194bd8e909bfa8c19bde
Mehakpal(Paras) <sharma.paras4444@gmail.com>
2020-07-30 13:42:50 +0530

    changing comments to content

--- app/controllers/wiki_comments_controller.rb
+++ app/controllers/wiki_comments_controller.rb
@@ -9,7 +9,7 @@ class WikiCommentsController < ApplicationController
   	puts "----#{@wiki_page.id}"
   	@user = User.find(params[:user_id])
     if @user.present? 
-  		@comment = @wiki_page.comments.new(commented_type: "WikiPage", commented_id: @wiki_page.id, author_id: @user.id, comments: params[:wiki_page][:comment])
+  		@comment = @wiki_page.comments.new(commented_type: "WikiPage", commented_id: @wiki_page.id, author_id: @user.id, content: params[:wiki_page][:comment])
   		if @comment.save
         @wiki_pages_comments = @wiki_page.comments
         flash[:notice] = l(:notice_successful_create)
